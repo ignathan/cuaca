c74b4909230c80e14226d40198a186385e2f45f2
Ignatius Nathan <ignatiusnathankesuma@gmail.com>
2022-10-03 16:20:19 +0800

    Build home screen with search bar

--- Cuaca/HomeViewController.swift
+++ Cuaca/HomeViewController.swift
@@ -8,4 +8,87 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.autocapitalizationType = .words
+        searchBar.delegate = self
+        searchBar.placeholder = "Search or enter city name"
+        return searchBar
+    }()
+    
+    var bottomConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .primaryBackground
+        view.addSubview(searchBar)
+        
+        bottomConstraint = searchBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        
+        NSLayoutConstraint.activate([
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomConstraint
+        ])
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification)
+    {
+        animateWithKeyboard(notification: notification) { keyboardFrame in
+            self.bottomConstraint.constant = -keyboardFrame.height
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification)
+    {
+        animateWithKeyboard(notification: notification) { keyboardFrame in
+            self.bottomConstraint.constant = 0
+        }
+    }
+    
+    func animateWithKeyboard(notification: NSNotification,
+                             animations: ((_ keyboardFrame: CGRect) -> Void)?)
+    {
+        let durationKey = UIResponder.keyboardAnimationDurationUserInfoKey
+        let duration = notification.userInfo![durationKey] as! Double
+        
+        let frameKey = UIResponder.keyboardFrameEndUserInfoKey
+        let keyboardFrameValue = notification.userInfo![frameKey] as! NSValue
+        
+        let curveKey = UIResponder.keyboardAnimationCurveUserInfoKey
+        let curveValue = notification.userInfo![curveKey] as! Int
+        let curve = UIView.AnimationCurve(rawValue: curveValue)!
+        
+        let animator = UIViewPropertyAnimator(
+            duration: duration,
+            curve: curve
+        ) {
+            animations?(keyboardFrameValue.cgRectValue)
+            
+            self.view?.layoutIfNeeded()
+        }
+        animator.startAnimation()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
+}
+
+// MARK: - UISearchBarDelegate
+extension HomeViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
 }
---
+++ Cuaca/Utility/UIColor+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extensions.swift
+//  Cuaca
+//
+//  Created by Ignatius Nathan on 03/10/2022.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var primaryBackground: UIColor { UIColor(named: "PrimaryBackground")! }
+
+    static var primaryText: UIColor { UIColor(named: "PrimaryText")! }
+}
